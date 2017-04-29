2226be893af7ba040736f571c03334dae7c1864b
Mantas Domaševičius <mantas.domasevicius@funguide.lt>
2017-04-29 17:28:40 +0300

    Remove redux-form

--- app-ui/src/root-reducer.js
+++ app-ui/src/root-reducer.js
@@ -1,12 +1,10 @@
 import {combineReducers} from 'redux';
 import {routerReducer} from 'react-router-redux';
-import {reducer as formReducer} from 'redux-form';
 import currencyConversionState from './app/currency-conversion-reducer';
 
 const rootReducer = combineReducers({
   currencyConversionState,
   routing: routerReducer,
-  form: formReducer
 });
 
 export default rootReducer;
