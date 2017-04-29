ee220c166f9137d886d56ae168813cd15a0c1043
Mantas Domaševičius <mantas.domasevicius@funguide.lt>
2017-04-29 17:29:00 +0300

    Fix source mapping for chrome

--- app-ui/src/configure-store.js
+++ app-ui/src/configure-store.js
@@ -11,7 +11,7 @@ function configureStoreProd(initialState) {
 
     // thunk middleware can also accept an extra argument to be passed to each thunk action
     // https://github.com/gaearon/redux-thunk#injecting-a-custom-argument
-      promise(),
+    promise(),
     thunk,
   ];
 
@@ -27,8 +27,8 @@ function configureStoreDev(initialState) {
 
     // Redux middleware that spits an error on you when you try to mutate your state either inside a dispatch or between dispatches.
     reduxImmutableStateInvariant(),
-      promise(),
-      logger,
+    promise(),
+    logger,
     // thunk middleware can also accept an extra argument to be passed to each thunk action
     // https://github.com/gaearon/redux-thunk#injecting-a-custom-argument
     thunk,
