187a75500bc22266391ad9aee205089fc5442c5e
Mantas Domaševičius <mantas.domasevicius@funguide.lt>
2017-04-30 11:02:02 +0300

    Test API index properly

--- acceptance/src/test/groovy/lt/danske/currency/api/ApiIndexSpec.groovy
+++ acceptance/src/test/groovy/lt/danske/currency/api/ApiIndexSpec.groovy
@@ -14,7 +14,11 @@ class ApiIndexSpec extends Specification {
             def response = rest.get('/api')
         then:
             response.status == 200
-            response.body._links.size() == 3
+            response.body._links.size() == 4
+            response.body._links.convert
+            response.body._links.purchase
+            response.body._links.availableCurrencies
+            response.body._links.'10DayExchangeRateHistory'
     }
 
 }
